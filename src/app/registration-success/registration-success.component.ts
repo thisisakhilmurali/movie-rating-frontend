18602668355d1f6cf2e0ae987faee5b72602ac0e
Akhil Murali <63278163+akhilmuraliai@users.noreply.github.com>
2023-06-26 22:34:08 +0530

    Added page for registration success

---
+++ src/app/registration-success/registration-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-success',
+  templateUrl: './registration-success.component.html',
+  styleUrls: ['./registration-success.component.css']
+})
+export class RegistrationSuccessComponent {
+
+}
