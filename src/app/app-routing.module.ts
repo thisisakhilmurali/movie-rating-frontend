dbf97a350c68a996a889f075f038d13ca23f59be
taniyaelizabethmax2000 <taniyaelizabeth.max@ust.com>
2023-06-28 09:58:36 +0530

    committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { HomeComponent } from './home/home.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {path:'login',component:AuthenticationComponent},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'admin-dash',component:AdminDashboardComponent}
+
+  
  
 ];
 @NgModule({
--- src/app/user-movie-component/user-movie-component.component.ts
+++ src/app/user-movie-component/user-movie-component.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-movie-component',
   templateUrl: './user-movie-component.component.html',
   styleUrls: ['./user-movie-component.component.css']
 })
-export class UserMovieComponentComponent {
+export class UserMovieComponentComponent 
+// implements OnInit 
+{
+ 
+  // constructor() { }
+
+  // ngOnInit(): void {
+    
+  // }
+
+  // reviewMessage: string = '';
+  // starValue: number = 0;
+
+  // submitReview() {
+  //   console.log(this.reviewMessage);
+  //   console.log(this.starValue);
+  // }
+
+  // clearAllData() {
+  //   this.reviewMessage = '';
+  //   this.starValue = 0;
+  // }
 
 }
